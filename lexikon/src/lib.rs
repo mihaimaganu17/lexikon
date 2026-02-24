f039941dcd7b7b8b4d869b7836a2a2375fc5df4e
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-02-24 09:13:43 +0100

    Successful socket call

--- lexikon/src/lib.rs
+++ lexikon/src/lib.rs
@@ -1,5 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+unsafe extern "C" {
+    fn socket(domain: i32, t_type: i32, protocol: i32) -> i32;
+}
+
+/// Comprises types of communication domains whithin which communication will take place. These
+/// are also regarded as address families (AF_)
+pub(crate) mod domain {
+    // Used by Internet Protocols, IPv4: TCP, UDP
+    pub const AF_INET: i32 = 2;
+}
+
+/// Types of sockets
+mod socket_type {
+    // Stream socket: TCP
+    pub const SOCK_STREAM: i32 = 1;
+    // Datagram socket: UDP
+    pub const SOCK_DGRAM: i32 = 2;
 }
 
 #[cfg(test)]
@@ -7,8 +22,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_socket() {
+        let fd = unsafe {
+            socket(domain::AF_INET, socket_type::SOCK_STREAM, 0)
+        };
+        println!("{}", fd);
+        assert!(fd != -1);
     }
 }
