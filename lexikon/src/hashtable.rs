761e946a2d09a67da0048297f39620a0e5d72558
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-03-13 18:32:04 +0100

    HashTable with init and size check

--- lexikon/src/hashtable.rs
+++ lexikon/src/hashtable.rs
@@ -8,6 +8,41 @@ struct HNode {
     hcode: u64,
 }
 
+#[derive(Debug, Default)]
+struct HashTable {
+    // Pointer to the hashtable
+    tab: *const *const HNode,
+    // Mask to map the hash according to our desired size
+    mask: usize,
+    // Number of keys currently in the table
+    size: usize,
+}
+
+impl HashTable {
+    pub fn init(size: usize) -> Result<Self, HashTableError> {
+        // Make sure the size is not 0 or negative. `usize` prevents this but we are extra
+        if size <= 0 {
+            return Err(HashTableError::NegativeSize);
+        }
+        // Make sure size is a power of 2
+        if size - 1 & size != 0 {
+            return Err(HashTableError::SizeNotPowerOfTwo(size));
+        }
+
+
+        Ok(Self {
+            size,
+            ..Self::default()
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum HashTableError {
+    NegativeSize,
+    SizeNotPowerOfTwo(usize),
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
