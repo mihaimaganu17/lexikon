e327f63afec74d1002b81e65cfb5354f7c0ece2a
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-03-22 19:42:01 +0100

    Added test for container of

--- lexikon/src/mem.rs
+++ lexikon/src/mem.rs
@@ -12,8 +12,17 @@ macro_rules! offset_of {
 
 macro_rules! field_ptr {
     ($base_ptr:expr, $base_type:path, $field:tt) => {{
+        // Check the field is in the base type. This issues a compile time error if `$field` is not
+        // a member of `$base_type`
+        let $base_type { $field: _, .. };
         let field_ptr = unsafe { &raw const (*($base_ptr as *const $base_type)).$field };
         field_ptr
+    }};
+}
+
+macro_rules! container_of {
+    ($field_ptr:expr, $base_type:path, $field:tt) => {{
+        core::ptr::null_mut()
     }};
 }
 
@@ -22,7 +31,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_container() {
+    fn container_of() {
+        #[repr(C)]
+        struct Node {
+            a: u32,
+            b: u32,
+        }
+
+        let node_ptr: *mut Node = Box::into_raw(Box::new(Node {
+            a: 0xb00b,
+            b: 0x1337,
+        }));
+
+        #[repr(C)]
+        struct Entry {
+            before_node: u64,
+            node: *mut Node,
+        }
+
+        let entry_orig_ptr = Box::into_raw(Box::new(Entry { before_node: 0xa01e0, node:node_ptr }));
+
+        let entry_container: *mut Entry = container_of!(node_ptr, Entry, node);
+        println!("{:#?}", entry_orig_ptr);
+        println!("{:#?}", entry_container);
+
+        assert!(entry_orig_ptr == entry_container);
+    }
+
+    #[test]
+    fn offset_of() {
         #[repr(C)]
         struct Node {
             a: u32,
